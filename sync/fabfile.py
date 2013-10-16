22fbf027c132353b473429f1949f7456698611bf
Gregory Eric Sanderson <gzou2000@gmail.com>
2013-10-15 21:13:01 -0400

    sync example now uses an isolated git repo

--- sync/fabfile.py
+++ sync/fabfile.py
@@ -1,6 +1,6 @@
 from fabric.api import *
 
-DEFAULT_PATH = '/home/gregory/opencode'
+DEFAULT_PATH = '/home/gregory/opencode/tmp/testrepo'
 DEPLOY_BRANCH = 'deploy'
 
 
@@ -11,15 +11,16 @@ def sync(path=DEFAULT_PATH):
 
 
 def add(path=DEFAULT_PATH):
-    with cd(path):
+    with lcd(path):
+        local('pwd')
         local('git add -i')
 
 
 def commit(path=DEFAULT_PATH):
-    with cd(path):
+    with lcd(path):
         local('git commit -v')
 
 
 def push(path=DEFAULT_PATH):
-    with cd(path):
+    with lcd(path):
         local('git push origin master:%s' % DEPLOY_BRANCH)
